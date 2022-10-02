7d4fbb0a1095957ee063aa30b3670961a3e399cd
BamseyD <profbamsey@gmail.com>
2022-10-02 16:28:35 +0100

    Setting up the Layout Manager

--- Notepad/src/NotePad.java
+++ Notepad/src/NotePad.java
@@ -3,10 +3,12 @@ import javax.swing.filechooser.FileFilter;
 import javax.swing.plaf.InsetsUIResource;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.GridBagConstraints;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+
 
 public class NotePad extends JFrame {
    //attributes
@@ -41,10 +43,79 @@ public class NotePad extends JFrame {
         setSize(700,500);
         //set window close operation to close app
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-
-
         
+        //make menu
+        JMenuBar menuBar = new JMenuBar(); //create Jmenu object
+        setJMenuBar(menuBar); //set it as main bar for this app
+        //make menu list
+        JMenu menu = new JMenu("File"); //create Jmenu (top lv menu e.g home, insert)
+        menu.setMnemonic(KeyEvent.VK_F); //keyboard shortcut for menu (alt-F)
+        menu.getAccessibleContext().setAccessibleDescription("File menu"); //accessability feature
+        menuBar.add(menu);
+
+        //MENU ITEMS
+        //new menu item
+        JMenuItem newMenu = new JMenuItem("New", KeyEvent.VK_N); //instantiate new JMenuItem object. indicating shortcut is crlt-n
+        newMenu.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N,ActionEvent.CTRL_MASK)); //set shortcut crlt-n
+        newMenu.getAccessibleContext().setAccessibleDescription("Loads a file"); //text shown whne hover over item
+        newMenu.addActionListener(new ActionListener() { //create new ActionListener object and within method is overriden (defines what happens to core control action - mouse click occurs) 
+            public void actionPerformed(ActionEvent e) {
+                //TODO new file method call
+            }
+        });
+        //add to the file menu
+        menu.add(newMenu);
+
+        //load menu item
+        JMenuItem loadMenu = new JMenuItem("Load", KeyEvent.VK_L); 
+        loadMenu.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_L,ActionEvent.CTRL_MASK)); 
+        loadMenu.getAccessibleContext().setAccessibleDescription("Loads a file");
+        loadMenu.addActionListener(new ActionListener() { 
+            public void actionPerformed(ActionEvent e) {
+                //TODO new file method call
+            }
+        });
+        //add to the file menu
+        menu.add(loadMenu);
+
+        //save menu item
+        JMenuItem saveMenu = new JMenuItem("Save", KeyEvent.VK_S); 
+        saveMenu.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S,ActionEvent.CTRL_MASK)); 
+        saveMenu.getAccessibleContext().setAccessibleDescription("Save the current file");
+        saveMenu.addActionListener(new ActionListener() { 
+            public void actionPerformed(ActionEvent e) {
+                //TODO new file method call
+            }
+        });
+        //add to the file menu
+        menu.add(saveMenu);
+
+        //save as menu item
+        JMenuItem saveAsMenu = new JMenuItem("Save As", KeyEvent.VK_A); 
+        saveAsMenu.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A,ActionEvent.CTRL_MASK)); 
+        saveAsMenu.getAccessibleContext().setAccessibleDescription("Save the current file");
+        saveAsMenu.addActionListener(new ActionListener() { 
+            public void actionPerformed(ActionEvent e) {
+                //TODO new file method call
+            }
+        });
+        //add to the file menu
+        menu.add(saveAsMenu);
+
+        //SET LAYOUT MANAGER
+        setLayout(new GridBagLayout()); //sets app LM to GridBagsLayout
+        GridBagConstrainsts constraints = new GridBagConstrainsts(); //instantiate GridBagCon object. Use to control placemnt of controls within grid.
+        //set constraints
+        constraints.gridx = 0; //specifiy which column and row the first control will be placed.
+        constraints.gridy = 0;
+        constraints.weightx = 1; //inform each cell in grid how expand. expand horizontally x but not vertically y
+        constraints.weighty = 0;
+        constraints.fill = GridBagConstrainsts.HORIZONTAL; //inform cell how fill available space. this instance horizonatlly
+
+
+
+
+
         //draw Jframe (test)
         setVisible(true);
     }
